42a0bd06ae882e04fce7e93955e4dc245ed56a83
Gabrielly Souza <gabymonteiro197@gmail.com>
2023-10-05 23:56:36 +0100

    at

--- aula12exe/exe.js
+++ aula12exe/exe.js
@@ -1,20 +1,33 @@
 var agora = new Date()
-var hora = 8
+var hora = agora.getHours()
 var minuto = agora.getMinutes()
 var hrescr = document.getElementById('horaescrita')
 var img = document.getElementById('imagem')
-var corpo = document.getElementById('body')
 hrescr.innerHTML = `<p>Agora são ${hora}:${minuto}.</p>`
-if (hora > 5 && hora < 12){
-    corpo.style.background = 'rgb(206, 177, 105)'
+
+function inseririmagem(url) {
+    var img = document.createElement('img')
+    img.src = url
+    img.style.width= "300px"
+    img.style.height= "300px"
+    img.style.borderRadius = "200px"
+    document.getElementById('imagem').appendChild(img)
+}
+
+if (hora >= 5 && hora < 12){
+    document.body.style.background = 'rgb(206, 177, 105)'
+    inseririmagem("imagens/manha.jpg")
 
 }
-else if (hora > 12 && hora < 18){
-    corpo.style.background = 'rgb(184, 125, 80)'
+else if (hora >= 12 && hora < 18){
+    document.body.style.background = 'rgb(184, 125, 80)'
+    inseririmagem("imagens/tarde.jpg")
 }
-else if(hora > 19 && hora < 23){
-    corpo.style.background = 'rgb(40, 40, 40)'
+else if(hora >= 18 && hora <= 23){
+    document.body.style.background = 'rgb(40, 40, 40)'
+    inseririmagem("imagens/noite.jpg")
 }
 else {
-    corpo.style.background = 'rgb(29, 29, 26)'
+    document.body.style.background = 'rgb(29, 29, 26)'
+    inseririmagem("imagens/madrugada.jpg")
 }
